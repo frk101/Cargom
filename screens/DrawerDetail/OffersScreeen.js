1791ce43a3b5a9f5945b732fe96a39e1728c1ef0
sevketaydogdu <44119231+sevketaydogdu@users.noreply.github.com>
2021-05-30 22:07:54 +0300

    Teklifler Sayfası tasarımı

--- screens/DrawerDetail/OffersScreeen.js
+++ screens/DrawerDetail/OffersScreeen.js
@@ -1,17 +1,64 @@
-import React from "react";
-import { StyleSheet, Text, View, FlatList, Image } from "react-native";
+import React,{useState} from "react";
+import { StyleSheet, Text, View, FlatList, Image,TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import COLORS from "../../constans/colors";
 import { Appbar } from "react-native-paper";
+import { ListItem } from "react-native-elements";
+
+
+import myData from "../../data/FakeData";
+
 
 const OffersScreeen = () => {
+  const [aktif,setAktif]=useState();
   return (
-    <View style={{ backgroundColor: "#FFFFFF", flex: 1 }}>
+    <View style={{ backgroundColor: "#FFFFFF", flex: 1, }}>
       <Headers />
+        <View style={styles.tabsWrapper}>
+        <View style={styles.aktifTabStyle}>
+          <Text style={styles.aktifTabText}>Aktif Tab</Text>
+        </View>
+        <View style={styles.pasifTabStyle}>
+        <Text style={styles.pasifTabText}>Pasif Tab</Text>
+
+        </View>
+
+      </View>
+      <FlatList
+        data={myData}
+        renderItem={({ item }) => <RenderList item={item} />}
+      />
+      
     </View>
   );
 };
+
+const RenderList = ({ item }) => {
+  const navigation = useNavigation();
+  return (
+    <TouchableOpacity
+      style={styles.listContainer}
+      onPress={() => navigation.navigate("AllCargoDetail", item)}
+    >
+      <ListItem bottomDivider>
+        <Image source={item.icon} style={{ width: 40, height: 40 }} />
+        <ListItem.Content>
+          <ListItem.Title style={{ color: COLORS.text, fontWeight: "bold" }}>
+            {item.name}
+          </ListItem.Title>
+          <ListItem.Title style={{ color: COLORS.text }}>
+            {item.km}
+          </ListItem.Title>
+        </ListItem.Content>
+        <ListItem.Title style={{ color: COLORS.primary, fontWeight: "bold" }}>
+          {item.ücret}
+        </ListItem.Title>
+      </ListItem>
+    </TouchableOpacity>
+  );
+};
+
 const Headers = () => {
   const navigation = useNavigation();
   const _goBack = () => navigation.goBack();
@@ -28,4 +75,38 @@ const Headers = () => {
 
 export default OffersScreeen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  tabsWrapper:{
+    flex:1,
+    flexDirection:'row',
+    marginTop:20,
+    maxHeight:48,
+    marginHorizontal:20
+  },
+  aktifTabStyle:{
+    flex:1,
+    backgroundColor:COLORS.primary,
+    marginRight:8,
+    borderRadius:80,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  aktifTabText:{
+    color: "#ffffff", 
+    fontSize: 20, 
+    fontWeight: "bold"
+  },
+  pasifTabStyle:{
+    flex:1,
+    backgroundColor:COLORS.lightGray,
+    marginLeft:8,
+    borderRadius:80,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  pasifTabText:{
+    color: COLORS.gray, 
+    fontSize: 20, 
+    fontWeight: "bold"
+  }
+});
